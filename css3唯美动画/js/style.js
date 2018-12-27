66621164383ed543e1d2304d9b745b2a3ff7a844
VeryGoodYou <1984898516@qq.com>
2018-12-27 11:16:34 +0800

    2018.12.27

--- css3唯美动画/js/style.js
+++ css3唯美动画/js/style.js
@@ -1,12 +1,11 @@
-/*
-var cc = new Vue({
-    el:'#cc',
-    data:{
-        message:'3Q',
-    }
-});*/
 
-/*
-$(window).on('load',function () {
-    $('body').fadeIn(1500);
-});*/
+var qie = document.getElementById('list').getElementsByTagName('img');
+var img = document.getElementById('qie-1');
+for (var i = 0; i < qie.length; i++) {
+    qie[i].onmouseover = function () {
+        img.src = this.src;
+    };
+    img[i].onmouseout = function () {
+
+    }
+}
